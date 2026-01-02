a26c577098b4fc3221bf7794700a0158ca44c9b4
suman <myselfsumann@gmail.com>
2026-01-03 03:18:50 +0530

    Implemented : validator for chcekin/out payload

--- backend/src/routes/parking.routes.js
+++ backend/src/routes/parking.routes.js
@@ -2,8 +2,8 @@ import { checkInVehicle } from '../controllers/checkin.controller.js';
 import { checkOutVehicle } from '../controllers/checkout.controller.js';
 import { getParkingList } from '../controllers/parkingList.controller.js';
 
-router.post('/check-in', checkInVehicle);
-router.post('/check-out', checkOutVehicle);
+router.post('/check-in',validate(checkInSchema), checkInVehicle);
+router.post('/check-out', validate(checkOutSchema), checkOutVehicle);
 router.get('/active', getParkingList);
 
 export default router;
