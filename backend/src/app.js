89833195a408ab5c7ce37de6ee3f3848a06dbf02
suman <myselfsumann@gmail.com>
2026-01-03 03:34:08 +0530

    Added : documentations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+import errorMiddleware from './middlewares/error.middleware.js';
 import routes from './routes/index.js';
 
 const app = express();
@@ -8,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
+
+const swaggerDocument = YAML.load('./documentation/swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(errorMiddleware);
 
 export default app;
