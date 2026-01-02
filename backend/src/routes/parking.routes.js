155df9430b687bd62256e6e75f9a5cad606b49fb
suman <myselfsumann@gmail.com>
2026-01-03 04:27:27 +0530

    Fixed : parking routes imports

--- backend/src/routes/parking.routes.js
+++ backend/src/routes/parking.routes.js
@@ -1,6 +1,15 @@
+import express from 'express';
+
 import { checkInVehicle } from '../controllers/checkin.controller.js';
 import { checkOutVehicle } from '../controllers/checkout.controller.js';
 import { getParkingList } from '../controllers/parkingList.controller.js';
+
+import { validate } from '../middlewares/validate.middleware.js';
+
+import { checkInSchema } from '../validators/checkin.validator.js';
+import { checkOutSchema } from '../validators/checkout.validator.js';
+
+const router = express.Router();
 
 router.post('/check-in',validate(checkInSchema), checkInVehicle);
 router.post('/check-out', validate(checkOutSchema), checkOutVehicle);
